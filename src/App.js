4827e1b16f1432576ae8823adf791c460535189f
Yoidea <yoidea.cc@gmail.com>
2018-12-24 19:36:05 +0900

    Add sentence to Done

--- src/App.js
+++ src/App.js
@@ -33,7 +33,40 @@ class Done extends Component {
   render() {
     return (
       <div>
-        <h2>Done</h2>
+        <h1>登録完了</h1>
+        <p>お客様の会員登録が正常に完了しました。</p>
+        <p>3日以内に<strong>98,000円</strong>お支払い下さい。</p>
+        <p>お支払いが確認できない場合は<strong>法的処置</strong>をとらせていただきますのでご注意下さい。</p>
+        <h2>お問合せ</h2>
+        <table>
+          <tr>
+            <td>電話番号</td>
+            <td>0120-999-999</td>
+          </tr>
+          <tr>
+            <td>メールアドレス</td>
+            <td>info@example.com</td>
+          </tr>
+        </table>
+        <h2>お振込先</h2>
+        <table>
+          <tr>
+            <td>銀行名(銀行コード)</td>
+            <td>極道銀行(5901)</td>
+          </tr>
+          <tr>
+            <td>支店名(支店コード)</td>
+            <td>八草支店(893)</td>
+          </tr>
+          <tr>
+            <td>種別</td>
+            <td>普通預金</td>
+          </tr>
+          <tr>
+            <td>口座番号</td>
+            <td>12345678</td>
+          </tr>
+        </table>
       </div>
     );
   }
