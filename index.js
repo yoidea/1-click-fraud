4c59bd29fb990ba05c6458bb309e1ac2d2cc8abc
Yoidea <yoidea.cc@gmail.com>
2019-07-22 23:26:01 +0900

    :sparkles: Use react router dom

--- index.js
+++ index.js
@@ -1,24 +1,72 @@
-class HelloWorld extends React.Component {
+const { HashRouter, Route, Link } = window.ReactRouterDOM;
+
+class Invoice extends React.Component {
   render() {
     return (
-      <section className="section">
-        <div className="container">
-          <h1 className="title">Hello World</h1>
-          <p className="subtitle">
-            My first website with <strong>Bulma</strong>!
-          </p>
-        </div>
-      </section>
+      <div>
+        <section className="hero is-medium is-primary is-bold">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title is-1">登録完了</h1>
+              <p className="is-size-3">
+                お客様の会員登録が正常に完了しました。
+              </p>
+              <p className="is-size-3 has-text-danger">
+                3日以内に<span className="has-text-weight-bold">98,000円</span>
+                お支払い下さい。
+              </p>
+              <p className="is-size-3 has-text-danger">
+                お支払いが確認できない場合は
+                <span className="has-text-weight-bold">法的処置</span>
+                をとらせていただきますのでご注意下さい。
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="section">
+          <div className="container">
+            <h1 className="title">お問合せ</h1>
+            <table className="table is-striped is-fullwidth">
+              <tbody>
+                <tr>
+                  <td>電話番号</td>
+                  <td>0120-999-999</td>
+                </tr>
+                <tr>
+                  <td>メールアドレス</td>
+                  <td>info@example.com</td>
+                </tr>
+              </tbody>
+            </table>
+            <h1 className="title">お振込先</h1>
+            <table className="table is-striped is-fullwidth">
+              <tbody>
+                <tr>
+                  <td>銀行名（銀行コード）</td>
+                  <td>極道銀行（5910）</td>
+                </tr>
+                <tr>
+                  <td>支店名（支店コード）</td>
+                  <td>八草支店（893）</td>
+                </tr>
+                <tr>
+                  <td>種別</td>
+                  <td>普通預金</td>
+                </tr>
+                <tr>
+                  <td>口座番号</td>
+                  <td>59109393</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </section>
+      </div>
     );
   }
 }
 
 class Top extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { name: "" };
-  }
-
   render() {
     return (
       <section
@@ -35,27 +83,9 @@ class Top extends React.Component {
             <p className="is-size-3">
               そんな夢のような方法を教えます。あなたも億万長者になりましょう。
             </p>
-            <div className="field">
-              <div className="control">
-                <input
-                  className="input is-medium"
-                  type="text"
-                  value={this.state.name}
-                  onChange={e => {
-                    this.setState({ name: e.target.value });
-                  }}
-                  placeholder="お名前"
-                />
-              </div>
-            </div>
-            <button
-              className="button is-primary is-large"
-              onClick={() => {
-                location.href = `invoice.html#${this.state.name}`;
-              }}
-            >
+            <Link className="button is-primary is-large" to="invoice">
               もっと詳しく
-            </button>
+            </Link>
           </div>
         </div>
       </section>
@@ -65,7 +95,12 @@ class Top extends React.Component {
 
 class App extends React.Component {
   render() {
-    return <Top />;
+    return (
+      <HashRouter>
+        <Route exact path="/" component={Top} />
+        <Route exact path="/invoice" component={Invoice} />
+      </HashRouter>
+    );
   }
 }
 
